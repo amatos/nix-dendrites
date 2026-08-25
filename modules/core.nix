2b654268773c6fa6808840db460a99e92142ccdf
Alberth Matos <alberth@matos.cc>
2026-08-25 16:29:36 -0400

    fix: define both darwinConfigurations and nixosConfigurations as mergeable attrset options in modules/core.nix
    Signed-off-by: Alberth Matos <alberth@matos.cc>

--- modules/core.nix
+++ modules/core.nix
@@ -1,8 +1,24 @@
 { lib, ... }: {
-  options.dendritic = {
-    sharedSystemConfig = lib.mkOption {
-      type = lib.types.listOf lib.types.deferredModule;
-      default = [];
+  options = {
+    dendritic = {
+      sharedSystemConfig = lib.mkOption {
+        type = lib.types.listOf lib.types.deferredModule;
+        default = [];
+      };
+      userHomeConfig = lib.mkOption {
+        type = lib.types.listOf lib.types.deferredModule;
+        default = [];
+      };
+    };
+
+    flake.darwinConfigurations = lib.mkOption {
+      type = lib.types.attrsOf lib.types.raw;
+      default = {};
+    };
+
+    flake.nixosConfigurations = lib.mkOption {
+      type = lib.types.attrsOf lib.types.raw;
+      default = {};
     };
   };
 }
