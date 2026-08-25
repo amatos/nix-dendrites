8903ce2cd9a53446748a64becf783c7e2b4d4d09
Alberth Matos <alberth@matos.cc>
2026-08-25 16:33:21 -0400

    feat: added packages to nix.nix
    added:
    - nh
    - nil
    - nixd
    
    Signed-off-by: Alberth Matos <alberth@matos.cc>

--- modules/features/nix.nix
+++ modules/features/nix.nix
@@ -1,7 +1,12 @@
 { ... }: {
   dendritic.sharedSystemConfig = [
-    ({ ... }: {
+    ({ pkgs, ... }: {
       nix.settings.experimental-features = [ "nix-command" "flakes" ];
+      environment.systemPackages = [
+        pkgs.nh
+        pkgs.nil
+        pkgs.nixd
+      ];
     })
   ];
 }
